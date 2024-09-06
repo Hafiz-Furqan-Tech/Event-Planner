fb8960f644effc78397f41257634629eff5f7ba9
Hafiz-Furqan-Tech <hafizm.furqan456@gmail.com>
2024-09-06 22:34:53 +0500

    App testing

--- script.js
+++ script.js
@@ -1,150 +1,3 @@
-// import {
-//   getDocs,
-//   collection,
-//   db,
-//   auth,
-//   onAuthStateChanged,
-//   signOut,
-//   doc,
-//   deleteDoc,
-// } from "./Firebase/Firebase.js";
-
-// try {
-//   const eventsCards = document.getElementById("events-card");
-//   const eventCreate = document.getElementById("eventCreate");
-//   const loader = document.getElementById("loader");
-//   const login = document.getElementById("signup");
-//   const signup = document.getElementById("login");
-//   const Logout = document.getElementById("Logout");
-//   const LogoutItem = document.getElementById("LogoutItem");
-//   // const eventToast = document.getElementById("eventToast");
-
-//   function checkOnlineStatus() {
-//     if (navigator.onLine) {
-//       if (sessionStorage.getItem("redirectedFromOffline") === "true") {
-//         sessionStorage.removeItem("redirectedFromOffline");
-//         window.location.href = "index.html";
-//         getAllEvents();
-//       }
-//     } else {
-//       sessionStorage.setItem("redirectedFromOffline", "true");
-//       window.location.href = "./Check Internet/index.html";
-//     }
-//   }
-//   window.addEventListener("online", checkOnlineStatus);
-//   window.addEventListener("offline", checkOnlineStatus);
-
-//   onAuthStateChanged(auth, (user) => {
-//     if (user) {
-//       eventCreate.firstElementChild.setAttribute("href", "/create events/events.html");
-//       login.style.display = "none";
-//       signup.style.display = "none";
-//       LogoutItem.style.display = "block";
-//     } else {
-//       eventValidation();
-//       LogoutItem.style.display = "none";
-//       login.style.display = "block";
-//       signup.style.display = "block";
-//     }
-//   });
-
-//   getAllEvents();
-//   function eventValidation() {
-//     eventCreate.firstElementChild.addEventListener("click", (e) => {
-//       e.preventDefault();
-//       var toastEl = document.getElementById("eventToast");
-//       console.log(toastEl);
-//       if (toastEl) {
-//         var toast = new bootstrap.Toast(toastEl);
-//         toast.show();
-//       }
-//       eventCreate.firstElementChild.setAttribute("href", "index.html");
-//     });
-//   }
-
-//   async function getAllEvents() {
-//     loader.style.display = "flex";
-//     try {
-//       const querySnapshot = await getDocs(collection(db, "events"));
-//       eventsCards.innerHTML = "";
-//       querySnapshot.forEach((doc) => {
-//         const event = doc.data();
-//         const { banner, title, date, time, createdByEmail, location, desc } =
-//           event;
-//         const card = `<div class="col-4">
-//                       <div class="card my-4" id="card">
-//                         <img src="${banner}" class="card-img-top" alt="Event Image" />
-//                         <div class="card-body bg-dark text-white">
-//                           <h1 class="card-title"> Title : ${title} </h1>
-//                           <p class="card-text"> Description : ${desc} </p>
-//                         </div>
-//                         <ul class="list-group list-group-flush">
-//                           <li class="list-group-item bg-dark text-white"> Location : ${location} </li>
-//                           <li class="list-group-item bg-dark text-white"> Email : ${createdByEmail} </li>
-//                           <li class="list-group-item bg-dark text-white"> Time : ${time} </li>
-//                           <li class="list-group-item bg-dark text-white"> Date  : ${date} </li>
-//                         </ul>
-//                         <button class="position-sticky bottom-0 rounded-bottom-4 delete-event fs-5 fw-bold" data-id="${doc.id}">Delete Event</button>
-//                       </div>
-//                       </div>`;
-//         eventsCards.innerHTML += card;
-//       });
-
-//       deleteCard();
-//     } catch (error) {
-//       console.error("Error fetching events: ", error);
-//     } finally {
-//       loader.innerHTML = " ";
-//     }
-//   }
-
-//   Logout.addEventListener("click", (e) => {
-//     signOut(auth)
-//       .then(() => {
-//         e.preventDefault();
-//         var toastEl = document.querySelector("#toastLogout");
-//         if (toastEl) {
-//           var toast = new bootstrap.Toast(toastEl);
-//           toast.show();
-//         }
-//         setTimeout(() => {
-//           toastEl.classList.remove("show");
-//         }, 3000);
-//       })
-//       .catch((error) => {
-//         alert(error);
-//       });
-//   });
-
-//   function deleteCard() {
-//     const deleteButtons = document.querySelectorAll(".delete-event");
-
-//     deleteButtons.forEach((button) => {
-//       button.addEventListener("click", async (e) => {
-//         button.innerText = "Event deleting...";
-//         button.disabled = true;
-//         const documentId = button.getAttribute("data-id");
-
-//         const docRef = doc(db, "events", documentId);
-//         try {
-//           await deleteDoc(docRef);
-//           button.closest(".col-4").remove();
-//         } catch (error) {
-//           console.error("Error deleting document: ", error);
-//         } finally {
-//           button.innerText = "Delete Event";
-//           button.disabled = false;
-//         }
-//       });
-//     });
-//   }
-
-//   checkOnlineStatus();
-// } catch (error) {
-//   alert(error);
-// }
-
-// ==========================================================================================================
 import {
   getDocs,
   collection,
@@ -164,23 +17,23 @@ try {
   const login = document.querySelectorAll("#login");
   const Logout = document.querySelectorAll("#Logout");
   const LogoutItem = document.querySelectorAll("#LogoutItem");
-  const eventToast = document.getElementById("LoginToast"); // Added reference for the eventToast
-  const toastLogoutEl = document.querySelector("#toastLogout"); // Added reference for logout toast
+  const eventToast = document.getElementById("LoginToast"); 
+  const toastLogoutEl = document.querySelector("#toastLogout");
 
-  function checkOnlineStatus() {
-    if (navigator.onLine) {
-      if (sessionStorage.getItem("redirectedFromOffline") === "true") {
-        sessionStorage.removeItem("redirectedFromOffline");
-        // window.location.href = "index.html";
-        getAllEvents();
-      }
-    } else {
-      sessionStorage.setItem("redirectedFromOffline", "true");
-      // window.location.href = "./Check Internet/index.html";
-    }
-  }
-  window.addEventListener("online", checkOnlineStatus);
-  window.addEventListener("offline", checkOnlineStatus);
+  // function checkOnlineStatus() {
+  //   if (navigator.onLine) {
+  //     if (sessionStorage.getItem("redirectedFromOffline") === "true") {
+  //       sessionStorage.removeItem("redirectedFromOffline");
+  //       // window.location.href = "index.html";
+  //       getAllEvents();
+  //     }
+  //   } else {
+  //     sessionStorage.setItem("redirectedFromOffline", "true");
+  //     // window.location.href = "./Check Internet/index.html";
+  //   }
+  // }
+  // window.addEventListener("online", checkOnlineStatus);
+  // window.addEventListener("offline", checkOnlineStatus);
 
   onAuthStateChanged(auth, (user) => {
     if (user) {
@@ -199,18 +52,8 @@ try {
           item.style.display = "block";
         });
       });
-      // eventCreate.firstElementChild.setAttribute(
-      //   "href",
-      //   "/create events/events.html"
-      // );
-      // signup.style.display = "none";
-      // login.style.display = "none";
-      // LogoutItem.style.display = "block";
     } else {
-      eventValidation(); // This will handle toast if user is not logged in
-      // LogoutItem.style.display = "none";
-      // login.style.display = "block";
-      // signup.style.display = "block";
+      eventValidation(); 
       signup.forEach((signup) => {
         signup.style.display = "block";
       });
@@ -235,13 +78,6 @@ try {
         }
       });
     });
-    // eventCreate.firstElementChild.addEventListener("click", (e) => {
-    //   e.preventDefault();
-    //   if (eventToast) {
-    //     var toast = new bootstrap.Toast(eventToast);
-    //     toast.show();
-    //   }
-    // });
   }
 
   async function getAllEvents() {
@@ -300,27 +136,10 @@ try {
           }, 3000);
         })
         .catch((error) => {
-          alert(error);
+          console.log(error);
         });
     });
   });
-  // Logout.addEventListener("click", (e) => {
-  //   signOut(auth)
-  //     .then(() => {
-  //       e.preventDefault();
-  //       if (toastLogoutEl) {
-  //         var toast = new bootstrap.Toast(toastLogoutEl);
-  //         toast.show();
-  //       }
-  //       setTimeout(() => {
-  //         toastLogoutEl.classList.remove("show");
-  //       }, 3000);
-  //     })
-  //     .catch((error) => {
-  //       alert(error);
-  //     });
-  // });
-
   function deleteCard() {
     const deleteButtons = document.querySelectorAll(".deletebutton");
 
@@ -344,9 +163,8 @@ try {
     });
   }
 
-  checkOnlineStatus();
+  // checkOnlineStatus();
 } catch (error) {
   console.log(error);
 }
 
-// ==========================================================================================================
